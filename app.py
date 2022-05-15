153058f29a81d461aec1a565ac114bde5cbdd163
Vishwajeet Behera <vishwajeetbehera0@gmail.com>
2022-05-15 09:34:32 +0530

    push

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+
+import webbrowser
 import csv
 from flask import Flask, render_template,request,redirect,url_for
 import diseaseprediction
@@ -50,5 +52,9 @@ def drugs():
     medicine = request.form['medicine']
     return render_template('homepage.html',medicine=medicine,symptoms=symptoms)
 
+def open_browser():
+    webbrowser.open_new('http://127.0.0.1:5000/')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    open_browser()
+    app.run(port=5000)
